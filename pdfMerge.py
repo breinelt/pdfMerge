4072a8abd3007177feffdf56f21f1a55395ff2b8
breinelt <reibjoern@gmail.com>
2020-03-03 22:24:30 +0100

    Add todo comments and remove pdfs from repo

--- pdfMerge.py
+++ pdfMerge.py
@@ -2,14 +2,20 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 output = PdfFileWriter()
 
+# todo OpenFileDialog
+
 pdfEven = PdfFileReader(open('test2.pdf', 'rb'))
 pdfOdd = PdfFileReader(open('test.pdf', 'rb'))
+
+# todo assert same number of pages
 
 for i in range(pdfEven.getNumPages()):
     page = pdfEven.getPage(i)
     output.addPage(page)
     page = pdfOdd.getPage(pdfOdd.getNumPages() - 1 - i)
     output.addPage(page)
+
+# todo SaveFileDialog
 
 with open('newfile.pdf', 'wb') as f:
     output.write(f)
