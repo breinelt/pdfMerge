caf588e0d20bfa812f91fa3a65ff1653a1a6561c
breinelt <reibjoern@gmail.com>
2020-03-15 19:11:45 +0100

    Add pdf as default output filetype

--- pdfMerge.py
+++ pdfMerge.py
@@ -10,21 +10,22 @@ output = PdfFileWriter()
 
 # todo OpenFileDialog
 path_fronts = filedialog.askopenfilename(title='Fronts')
-path_backs = filedialog.askopenfilename(title= 'Backs')
-path_output = filedialog.asksaveasfilename(title= 'Output', defaultextension='.pdf')
+path_backs = filedialog.askopenfilename(title='Backs')
 
-pdfEven = PdfFileReader(open(path_fronts, 'rb'))
-pdfOdd = PdfFileReader(open(path_backs, 'rb'))
+pdfFronts = PdfFileReader(open(path_fronts, 'rb'))
+pdfBacks = PdfFileReader(open(path_backs, 'rb'))
 
 # todo assert same number of pages
+assert (pdfFronts.getNumPages() == pdfBacks.getNumPages()), 'Both documents have to have the same number of pages'
 
-for i in range(pdfEven.getNumPages()):
-    page = pdfEven.getPage(i)
-    output.addPage(page)
-    page = pdfOdd.getPage(pdfOdd.getNumPages() - 1 - i)
-    output.addPage(page)
+path_output = filedialog.asksaveasfilename(title='Output', filetypes=[('PDF files', '.pdf')], defaultextension='.pdf')
 
-# todo SaveFileDialog
+
+for i in range(pdfFronts.getNumPages()):
+    page = pdfFronts.getPage(i)
+    output.addPage(page)
+    page = pdfBacks.getPage(pdfBacks.getNumPages() - 1 - i)
+    output.addPage(page)
 
 with open(path_output, 'wb') as f:
     output.write(f)
