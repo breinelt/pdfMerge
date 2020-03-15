954c81235c63b8d69d6751f5fddc93c48f8519e5
breinelt <reibjoern@gmail.com>
2020-03-15 18:19:26 +0100

    Add fileopen and filesave dialogs

--- pdfMerge.py
+++ pdfMerge.py
@@ -1,11 +1,20 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
+import tkinter as tk
+from tkinter import filedialog
+
+
+root = tk.Tk()
+root.withdraw()
 
 output = PdfFileWriter()
 
 # todo OpenFileDialog
+path_fronts = filedialog.askopenfilename(title='Fronts')
+path_backs = filedialog.askopenfilename(title= 'Backs')
+path_output = filedialog.asksaveasfilename(title= 'Output', defaultextension='.pdf')
 
-pdfEven = PdfFileReader(open('test2.pdf', 'rb'))
-pdfOdd = PdfFileReader(open('test.pdf', 'rb'))
+pdfEven = PdfFileReader(open(path_fronts, 'rb'))
+pdfOdd = PdfFileReader(open(path_backs, 'rb'))
 
 # todo assert same number of pages
 
@@ -17,5 +26,5 @@ for i in range(pdfEven.getNumPages()):
 
 # todo SaveFileDialog
 
-with open('newfile.pdf', 'wb') as f:
+with open(path_output, 'wb') as f:
     output.write(f)
